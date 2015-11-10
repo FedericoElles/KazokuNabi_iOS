08030e6238f1c773ec955194ca8ff70a19880992
federicoelles <federico@gmail.com>
2015-11-10 12:49:00 +0100

    Initial Commit

---
+++ FamilyNavi Düsseldorf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FamilyNavi Düsseldorf
+//
+//  Created by Fritz on 10/11/15.
+//  Copyright © 2015 Fritz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
