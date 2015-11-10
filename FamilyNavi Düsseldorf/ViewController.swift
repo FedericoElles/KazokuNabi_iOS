e9371f640a54d46152ae1352f0fc6d1fc762e206
federicoelles <federico@gmail.com>
2015-11-10 19:37:40 +0100

    WebView can now communicate with Swift

--- FamilyNavi Düsseldorf/ViewController.swift
+++ FamilyNavi Düsseldorf/ViewController.swift
@@ -8,22 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
     
     
-
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
+    
     @IBOutlet weak var webview: UIWebView!
+    
+    func loadPage(){
+
+        
+        //let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
+        delay(3.0) {
+            // do stuff
+
+        }
+
+    }
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        // disable scroll out of edge
+        webview.scrollView.bounces = false
+        
+        webview.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
         let url = NSBundle.mainBundle().URLForResource("index", withExtension:"html", subdirectory: "html")
-        //let url = NSURL (string: "http://www.apple.com");
         let requestObj = NSURLRequest(URL: url!);
         webview.loadRequest(requestObj);
+        
     }
+    
+    func webViewDidStartLoad(wv: UIWebView) {
+        print("ViewDidStartLoad")
+    }
+    
+    
+    func webViewDidFinishLoad(webView: UIWebView)
+    {
+        print("ViewDidStopLoad")
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        let url = request.URL
+        let str = "\(url!)"
+        let urlArray = str.characters.split{$0 == ":"}.map(String.init)
+        if urlArray[0] == "swift" {
+            if urlArray[1] == "//ready" {
+                print("javascript:ready")
+                let result = self.webview.stringByEvaluatingJavaScriptFromString("window._sendLocationData('positionInitial', 51.249, 6.77576)")
+                print(result)
+            }
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    //BUTTONS
     
     @IBAction func back(sender: UIBarButtonItem) {
         webview.goBack()
